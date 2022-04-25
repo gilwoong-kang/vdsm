5083983aa221e0a5b4c59192c673defbe46370db
gilwoong-kang <smostr@gmail.com>
2022-04-25 17:30:11 +0900

    test

--- tests/lib/yajsonrpc/jsonrpcserver_test.py
+++ tests/lib/yajsonrpc/jsonrpcserver_test.py
@@ -187,7 +187,7 @@ class JsonRpcServerTests(TestCaseBase):
 
         with constructClient(self.log, bridge, ssl_ctx) as clientFactory:
             with self._client(clientFactory) as client:
-                self.assertEqual(self._callTimeout(client, "runInt",
+                self.assertEqual(self._callTimeout(client, "Host.runInt",
                                   {}, CALL_ID, data),"this is return")
 
     @permutations(USE_SSL)
