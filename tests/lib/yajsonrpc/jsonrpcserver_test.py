63298be99d056a7efaaf452b96a4c1cb6a5eb5e6
gilwoong-kang <smostr@gmail.com>
2022-04-25 18:22:13 +0900

    test

--- tests/lib/yajsonrpc/jsonrpcserver_test.py
+++ tests/lib/yajsonrpc/jsonrpcserver_test.py
@@ -192,7 +192,7 @@ class JsonRpcServerTests(TestCaseBase):
         with constructClient(self.log, bridge, ssl_ctx) as clientFactory:
             with self._client(clientFactory) as client:
                 self.assertEqual(self._callTimeout(client, "Host.runInt",
-                                  {}, CALL_ID, data))
+                                  {}, CALL_ID), data)
 
     @permutations(USE_SSL)
     @broken_on_ci("fails randomly in CI")
